a1b24e66b1642dbb2b8a861fee7038b9c18e0391
Daljeet Singh <daljeet.singh123@gmail.com>
2017-01-18 14:37:27 +0530

    further change to couch query

--- couch_query.js
+++ couch_query.js
@@ -6,31 +6,43 @@ var db = new(cradle.Connection)().database('my_database');
 
 //db.get('001',function(err,doc){console.log(doc); });
 
-db.view('employeeViews/age', function (err, res) {
-	console.log("printing all rows in the view : ");
+db.view('employeeViews/age',{reduce:false}, function (err, res) {
+	console.log("=====>printing all rows in the view : ");
 	console.log(res); 
 	res.forEach(function(key, row, id) {
 		console.log('%s: %s', key, row);
 	});
 });
 
-db.view('employeeViews/age',{key:24}, function (err, res) {
-	console.log("printing after filtering for age :24 "); 
+db.view('employeeViews/age',{reduce:false,key:24}, function (err, res) {
+	console.log("======> printing after filtering for age :24 "); 
 	res.forEach(function(key, row, id) {
 		console.log('%s: %s', key, row);
 	});
 });
 
-db.view('employeeViews/age',{descending:true}, function (err, res) {
-	console.log("printing in descending order "); 
+db.view('employeeViews/age',{descending:true,reduce:false}, function (err, res) {
+	console.log("=======> printing in descending order "); 
 	res.forEach(function(key, row, id) {
 		console.log('%s: %s', key, row);
 	});
 });
 
-db.view('employeeViews/age',{startkey:25}, function (err, res) {
-	console.log("printing all employees with age greater than equal to 25 "); 
-	res.forEach(function(key, row, id) {
-		console.log('%s: %s', key, row);
-	});
+	
+db.view('employeeViews/age',{startkey:25,reduce:true}, function (err, res) {
+	console.log("=======> printing all employees with age greater than equal to 25 "); 
+	console.log(res);
+	/*res.forEach(function(key, row, id) {
+		console.log('%s: %s', key, row);}*/
+	
 });
+
+	
+db.view('employeeViews/salary',{reduce:true}, function (err, res) {
+	console.log("=======> printing all employees with Salary View (total Salary payed by employee) "); 
+    console.log(res);
+	/*res.forEach(function(key, row, id) {
+		console.log('%s: %s', key, row);
+	} */
+   }); 
+   
